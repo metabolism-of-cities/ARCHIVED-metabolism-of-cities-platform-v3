2e799b944ee8577d471a3c0cbfdc9902813e1687
Paul Hoekman <paul@penguinprotocols.com>
2018-11-04 14:22:14 -0500

    Events 10

--- src/multiplicity/context_processors.py
+++ src/multiplicity/context_processors.py
@@ -1,5 +1,8 @@
 from django.contrib.sites.models import Site
+from core.models import Event
 
 def site(request):
     site = Site.objects.get_current()
-    return {'SITE_ID': site.id, 'SITE_URL': site.domain, 'SITE_NAME': site.name}
+    event = Event.objects.filter(article__site=site, start__lte='2006-01-01')[:1]
+
+    return {'SITE_ID': site.id, 'SITE_URL': site.domain, 'SITE_NAME': site.name, 'EVENT': event}
