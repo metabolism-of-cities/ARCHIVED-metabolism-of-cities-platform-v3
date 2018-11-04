98a2b9486e2870a510357567711a6d3e2e602a56
Paul Hoekman <paul@penguinprotocols.com>
2018-11-04 17:32:11 -0500

    Fully embedding last event in footer 20

--- src/multiplicity/context_processors.py
+++ src/multiplicity/context_processors.py
@@ -1,8 +1,10 @@
 from django.contrib.sites.models import Site
 from core.models import Event
+from datetime import datetime, timedelta, time
 
 def site(request):
     site = Site.objects.get_current()
-    event = Event.objects.filter(article__site=site, start__lte='2006-01-01')[:1]
+    today = datetime.now().date()
+    event = Event.objects.filter(article__site=site, start__gte=today).order_by('start').first()
 
     return {'SITE_ID': site.id, 'SITE_URL': site.domain, 'SITE_NAME': site.name, 'EVENT': event}
