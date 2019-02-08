7cf635d7ec136463b7d1fe0aa3db4b1905b36d5e
Paul Hoekman <paul@penguinprotocols.com>
2019-02-08 20:12:55 +0200

    Change model 3

--- src/core/models.py
+++ src/core/models.py
@@ -441,6 +441,7 @@ class Project(models.Model):
         ('bachelor', 'Bachelor'),
         ('masters', 'Master'),
         ('phd', 'PhD'),
+        ('other', 'Other'),
     )
     thesistype = models.CharField(max_length=20, choices=THESISTYPE, null=True, blank=True)
     site = models.ForeignKey(Site, on_delete=models.CASCADE)
