2fd8e4c375a719b7ea47b60f5bd64c401f952534
Paul Hoekman <paul@penguinprotocols.com>
2019-04-25 15:08:42 +0200

    OMAT rewrite 6

--- src/core/urls.py
+++ src/core/urls.py
@@ -42,6 +42,9 @@ urlpatterns = [
     path('mooc', RedirectView.as_view(url=site_url+'/resources/mooc', permanent=True)),
     path('journals', RedirectView.as_view(url=site_url+'/resources/journals', permanent=True)),
 
+    path('omat', RedirectView.as_view(url=site_url+'/resources/omat', permanent=True)),
+    path('omat/<slug:slug>', RedirectView.as_view(url=archive_url+'/omat/%(slug)s', permanent=True)),
+
     # Pending:
     path('research/<int:id>', RedirectView.as_view(url=site_url+'/community/research/projects', query_string=True, permanent=True)),
     path('news/<int:id>-<slug:slug>', RedirectView.as_view(url=site_url+'/community/news', permanent=True)),
